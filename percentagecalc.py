716ca4e27d5737730874e4ffca383bbddf09ba08
Sagar Sharma <78422995+Sagar-Sharma-7@users.noreply.github.com>
2021-08-24 09:10:47 +0530

    replaced float with int

--- percentagecalc.py
+++ percentagecalc.py
@@ -1,8 +1,8 @@
-n1=float(input("enter marks in english:"))
-n2=float(input("enter marks in hindi:"))
-n3=float(input("enter marks in maths:"))
-n4=float(input("enter marks in science:"))
-n5=float(input("enter marks in sst:"))
+n1= int(input("enter marks in english:"))
+n2= int(input("enter marks in hindi:"))
+n3= int(input("enter marks in maths:"))
+n4= int(input("enter marks in science:"))
+n5= int(input("enter marks in sst:"))
 per=(n1+n2+n3+n4+n5)/5
 if per >=85:
     print("A grade")
